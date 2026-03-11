91fc00c75c38387bf0b188312bb454b07fc6850f
vboxuser <vboxuser@WIN10>
2026-03-11 10:39:11 +0300

    Добавьте файлы проекта.

---
+++ KuznetsovAutoservice/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using KuznetsovAutoservice.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KuznetsovAutoservice
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static AutoServiceKuznetsovDBEntities context = new AutoServiceKuznetsovDBEntities();
+    }
+}
