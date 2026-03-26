fe1fe6e4c9e486a40c5f71275b36682dd35c658b
vboxuser <vboxuser@WIN10>
2026-03-26 10:37:02 +0300

    арп

--- KuznetsovAutoservice/View/Pages/AddEmpPage.xaml.cs
+++ KuznetsovAutoservice/View/Pages/AddEmpPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KuznetsovAutoservice.Class;
+using KuznetsovAutoservice.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,40 @@ namespace KuznetsovAutoservice.View.Pages
         public AddEmpPage()
         {
             InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameTb.Text) && string.IsNullOrEmpty(SurnameTb.Text) && string.IsNullOrEmpty(PositionTb.Text) && string.IsNullOrEmpty(SalaryTb.Text) && string.IsNullOrEmpty(LoginTb.Text) && string.IsNullOrEmpty(PasswordPb.Password))
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+
+            else
+            {
+                Employees employees = new Employees()
+                {
+                    FirstName = NameTb.Text,
+                    LastName = SurnameTb.Text,
+                    Position = PositionTb.Text,
+                    Salary = Convert.ToInt32(SalaryTb),
+                    Login = LoginTb.Text,
+                    Password = PasswordPb.Password
+
+                };
+
+
+
+                App.context.Employees.Add(employees);
+                App.context.SaveChanges();
+                MessageBox.Show("Акция добавлена");
+
+            }
+        }
+
+        private void MainBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.MainFrame.Navigate(new EmployeePage());
         }
     }
 }
